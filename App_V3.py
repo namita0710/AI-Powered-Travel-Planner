29d2c6f0964f6cc249034ff97dc2913c2c421bdc
namita0710 <68329002+namita0710@users.noreply.github.com>
2025-06-30 17:45:17 +0530

    Update App_V3.py
    Added two new features: theme based itinerary and View in maps

--- App_V3.py
+++ App_V3.py
@@ -5,7 +5,7 @@ import pandas as pd
 import tempfile
 
 # Configure Gemini API Key
-genai.configure(api_key="AIzaSyCO4qG0DWAHbetiiJRy0YoFgVcqfMX9L8U")
+genai.configure(api_key="Your API Key")
 
 # Function to generate itinerary
 def generate_itinerary(destination, days, interests, budget, theme):
